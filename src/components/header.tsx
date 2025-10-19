84942ad5a8719245d17dc502c2255739f46d6893
Mohammed Thouseef M <mthouseef100@gmail.com>
2025-10-19 15:52:57 +0000

    fix this in top (_for element header_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="w-full p-4 border-b-2 border-primary">
+    <header className="w-full p-4 border-b-2 border-primary flex items-baseline gap-4">
       <h1 className="text-2xl font-bold text-primary">Mark Gatere</h1>
       <p className="text-accent">Software Engineer</p>
     </header>
